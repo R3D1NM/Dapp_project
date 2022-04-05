7b0e8a85fdc5a61654a0bd9ba1c92191c12aa941
rinm.desktop <shinzeya@gmail.com>
2022-04-06 00:06:05 +0900

    build: connect web3

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,38 @@
-import React, { FC } from "react";
+import React, { FC,useEffect ,useState} from "react";
 import Main from "./routes/main";
 import {BrowserRouter, Routes,Route} from 'react-router-dom';
 const App: FC = () => {
+
+  const [Account, setAccount] = useState<string>("");
+
+  const getAccount =async () => {
+    try{
+      if(window.ethereum){
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+
+        setAccount(accounts[0]);
+      } else{
+        alert("Install METAMASK!")
+      }
+    }catch(err){
+    console.error(err);
+
+    }
+  }
+
+  useEffect(() => {
+    getAccount();
+  }, [])
+
+  useEffect(() => {
+    console.log(Account);
+    
+  }, [Account])
+  
+  
+
   return <BrowserRouter>
     <Routes>
       <Route path="/" element={<Main/>} />
